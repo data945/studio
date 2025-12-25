caf51bc07e98c5db15c358114379d4c17f1e91c8
Jafor Sadek <data@dlymfgusa.com>
2025-12-25 05:21:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3852240628-dea07",
+  "appId": "1:229079860382:web:59feedd46cdcddcc38a78f",
+  "apiKey": "AIzaSyBWzFmZXrzyOoKp6GT2Su8nPGHiulPzZEw",
+  "authDomain": "studio-3852240628-dea07.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "229079860382"
+};
